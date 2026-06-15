b8886c37ee911297cd37c47cf6fed45f2898d306
Bennett Dungan <bennettdungan@gmail.com>
2026-06-15 15:32:31 -0500

    fix output path error

--- .eleventy.js
+++ .eleventy.js
@@ -33,7 +33,7 @@ module.exports = function (eleventyConfig) {
 
   // Minify HTML output
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    if (outputPath.indexOf(".html") > -1) {
+    if (typeof outputPath === "string" && outputPath.endsWith(".html")) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
